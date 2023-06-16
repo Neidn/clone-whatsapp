6fed9553e1293f542ae9083ece287b88031b811a
Neidn <dty3152@gmail.com>
2023-06-16 11:09:08 +0900

    First Commit and Set firebase

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import '/firebase_options.dart';
+
+Future<void> init(WidgetsBinding widgetsBinding) async {
+  print('Initializing...');
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  print('Initializing complete!');
+}
+
+void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensurºeInitialized();
+  await init(widgetsBinding);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Demo Home Page'),
+        ),
+        body: const Center(
+          child: Text(
+            'This is a demo of the Material 3.0 theme!',
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+      ),
+    );
+  }
+}
