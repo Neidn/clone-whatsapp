fcc76bf367321d40dc34819b82481779470ba812
Neidn <dty3152@gmail.com>
2023-06-19 16:44:04 +0900

    create mobile_layout_screen.dart

--- lib/screens/mobile_layout_screen.dart
+++ lib/screens/mobile_layout_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '/common/utils/colors.dart';
+
 class MobileLayoutScreen extends ConsumerStatefulWidget {
   const MobileLayoutScreen({super.key});
 
@@ -8,9 +10,88 @@ class MobileLayoutScreen extends ConsumerStatefulWidget {
   ConsumerState<MobileLayoutScreen> createState() => _MobileLayoutScreenState();
 }
 
-class _MobileLayoutScreenState extends ConsumerState<MobileLayoutScreen> {
+class _MobileLayoutScreenState extends ConsumerState<MobileLayoutScreen>
+    with TickerProviderStateMixin {
+  late final TabController _tabBarController;
+
+  @override
+  void initState() {
+    _tabBarController = TabController(
+      length: 3,
+      vsync: this,
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _tabBarController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: appBarColor,
+        centerTitle: false,
+        title: const Text(
+          'WhatsApp',
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.grey,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search, color: Colors.grey),
+          ),
+          PopupMenuButton(
+            icon: const Icon(
+              Icons.more_vert,
+              color: Colors.grey,
+            ),
+            itemBuilder: (context) => [
+              PopupMenuItem(
+                onTap: () {},
+                child: const Text('Create group'),
+              ),
+            ],
+          ),
+        ],
+        bottom: TabBar(
+          controller: _tabBarController,
+          indicatorColor: tabColor,
+          indicatorWeight: 4,
+          labelColor: tabColor,
+          unselectedLabelColor: Colors.grey,
+          labelStyle: const TextStyle(fontWeight: FontWeight.bold),
+          tabs: const [
+            Tab(text: 'CHATS'),
+            Tab(text: 'STATUS'),
+            Tab(text: 'CALLS'),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabBarController,
+        children: [
+          const Center(child: Text('Chats')),
+          const Center(child: Text('Status')),
+          const Center(child: Text('Calls')),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: tabColor,
+        onPressed: () {},
+        child: const Icon(
+          Icons.comment,
+          color: Colors.white,
+        ),
+      ),
+    );
   }
 }
